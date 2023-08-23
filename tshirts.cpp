110d6a75a6107e952a95a920497841e2dca4070b
aananyasinha13 <141901040+aananyasinha13@users.noreply.github.com>
2023-08-23 14:59:39 +0530

    Add files via upload
    commit tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,34 +1,22 @@
 #include <iostream>
-#include "tshirts.h"
+#include <assert.h>
 
-char Tshirts::size(int cms)
-{
+char size(int cms) {
     char sizeName = '\0';
-    if (cms <= 38) {
+    if(cms <= 38) {
         sizeName = 'S';
-    }
-    else if (cms > 38 && cms <= 42) {
+    } else if(cms > 38 && cms <= 42) {
         sizeName = 'M';
-    }
-    else if (cms > 42) {
+    } else if(cms > 42) {
         sizeName = 'L';
     }
     return sizeName;
 }
 
-void TestTshirts::testforTshirtSizes()
-{
-    assert(Tshirts::size(37) == 'S');
-    assert(Tshirts::size(38) == 'S');
-    assert(Tshirts::size(40) == 'M');
-    assert(Tshirts::size(42) == 'M');
-    assert(Tshirts::size(43) == 'L');
-    std::cout << "All is well (maybe!)\n";
-}
-
 int main() {
-
-    TestTshirts::testforTshirtSizes();
-
+    assert(size(37) == 'S');
+    assert(size(40) == 'M');
+    assert(size(43) == 'L');
+    std::cout << "All is well (maybe!)\n";
     return 0;
 }
